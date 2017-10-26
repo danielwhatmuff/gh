27b30c5f8fe3f78957836b6bb35220c769c4da2d
Daniel Whatmuff <danielwhatmuff@gmail.com>
2017-10-26 13:35:01 +0100

    adding initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# gh
+#![](https://cdn.travis-ci.com/images/logos/TravisCI-Full-Color-45e242791b7752b745a7ae53f265acd4.png)
+
+## Overview
+* Install and execute to open a Github repo home page from the command line
+
+### Install the CLI and view the available options
+```yaml
+$ pip install gh
+$ gh --help
+```
+
+### Fork and PR with contributions
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Setup.py for gh
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='gh',
+    version='0.0.1',
+    description='',
+    long_description='A CLI to open Github project in a browser',
+    url='https://github.com/danielwhatmuff/gh',
+    author='Daniel Whatmuff',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='github cli open repo',
+    py_modules=["gh"],
+    install_requires=['requests'],
+    scripts=['bin/gh'],
+)
